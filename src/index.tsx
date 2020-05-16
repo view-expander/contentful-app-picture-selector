9c06dc4a04e368912da377e42f79a5f438a86bd3
KIMURA Tetsuro <haribote.nobody@gmail.com>
2020-05-17 00:21:48 +0900

    fix: type-error.

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,23 @@
 import '@contentful/forma-36-fcss/dist/styles.css'
 import '@contentful/forma-36-react-components/dist/styles.css'
-import { init, locations, AppExtensionSDK } from 'contentful-ui-extensions-sdk'
+import { AppExtensionSDK, init, locations } from 'contentful-ui-extensions-sdk'
 import type { DialogExtensionSDK, KnownSDK } from 'contentful-ui-extensions-sdk'
 import React from 'react'
 import { render } from 'react-dom'
 import { Config } from './components/Config'
 import { HelloWord } from './components/HelloWorld'
 
-const isConfig = (sdk: KnownSDK): sdk is AppExtensionSDK => sdk.location.is(locations.LOCATION_APP_CONFIG)
+const isConfig = (sdk: KnownSDK): sdk is AppExtensionSDK =>
+  sdk.location.is(locations.LOCATION_APP_CONFIG)
 const isDialog = (sdk: KnownSDK): sdk is DialogExtensionSDK =>
   sdk.location.is(locations.LOCATION_DIALOG)
 
+const Root: React.FC<{ sdk: KnownSDK }> = ({ sdk }) =>
+  isConfig(sdk) ? <Config sdk={sdk} /> : <HelloWord sdk={sdk} />
+
 init((sdk) => {
   console.log('init', sdk)
-  const Component = isConfig(sdk) ? Config : HelloWord
-  render(<Component sdk={sdk as any} />, document.getElementById('root'))
+  render(<Root sdk={sdk} />, document.getElementById('root'))
 
   if (isDialog(sdk)) {
     sdk.window.startAutoResizer()
