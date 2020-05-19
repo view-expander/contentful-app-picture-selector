54afc4ff088520c9032a20ac10eb041770e7de09
KIMURA Tetsuro <haribote.nobody@gmail.com>
2020-05-20 00:36:29 +0900

    feat: <Dialog />, debug code.

--- src/views/Dialog.tsx
+++ src/views/Dialog.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { DialogExtensionSDK } from 'contentful-ui-extensions-sdk'
 import { useAutoResize } from '../hooks/useAutoResize'
 import { Dump } from './Dump'
 
 export const Dialog: React.FC<{ sdk: DialogExtensionSDK }> = ({ sdk }) => {
   useAutoResize(sdk)
+  useEffect(() => {
+    console.log('params', sdk.parameters.invocation)
+  }, [sdk.parameters])
   return <Dump sdk={sdk} />
 }
