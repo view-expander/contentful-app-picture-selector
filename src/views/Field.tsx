4a7b4dce8db16f99bfa18142129421f56be16708
KIMURA Tetsuro <haribote.nobody@gmail.com>
2020-05-20 19:17:04 +0900

    fix: <Field />

--- src/views/Field.tsx
+++ src/views/Field.tsx
@@ -5,25 +5,17 @@ import styled from 'styled-components'
 import { useAutoResize } from '../hooks/useAutoResize'
 
 const ButtonLabel = styled.div`
-  display: inline-flex
-  align-items: center
+  display: inline-flex;
+  align-items: center;
 `
 
 export const Field: React.FC<{ sdk: FieldExtensionSDK }> = ({ sdk }) => {
   const [selectedItemList, setSelectedItemList] = useState<SelectedItemList>([])
   const onClickDialogOpener = (): Promise<void> =>
-    sdk.field
-      .setValue({ items: selectedItemList })
-      .then(() =>
-        console.log('Field.onClickDialogOpener()', sdk.field.getValue())
-      )
-      .then(() => new Promise((resolve) => setTimeout(resolve, 1000)))
-      .then(() =>
-        sdk.dialogs.openCurrentApp({
-          title: 'Picture Selector',
-          width: 'large',
-        })
-      )
+    sdk.dialogs.openCurrentApp({
+      title: 'Picture Selector',
+      width: 'large',
+    })
 
   useEffect(() => {
     const value = sdk.field.getValue() as
@@ -48,7 +40,7 @@ export const Field: React.FC<{ sdk: FieldExtensionSDK }> = ({ sdk }) => {
       </ul>
       <Button buttonType="muted" size="small" onClick={onClickDialogOpener}>
         <ButtonLabel>
-          <Icon color="muted" icon="Asset" />
+          <Icon color="muted" icon="Asset" />&nbsp;
           <span>Add pictures</span>
         </ButtonLabel>
       </Button>
