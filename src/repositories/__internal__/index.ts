09bd31cf42756a89b6b6beabec86771710928493
KIMURA Tetsuro <haribote.nobody@gmail.com>
2020-05-20 23:48:05 +0900

    change: http.

--- src/repositories/__internal__/index.ts
+++ src/repositories/__internal__/index.ts
@@ -1,6 +1,11 @@
 import axios from 'axios'
 
-const http = axios.create({ baseURL: process.env.API_PATH })
+const http = axios.create({
+  baseURL: process.env.API_PATH,
+  headers: {
+    'Access-Control-Allow-Origin': '*'
+  }
+})
 
 export class Repository {
   protected http = http
