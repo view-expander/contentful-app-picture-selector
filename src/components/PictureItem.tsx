a6362f8f69f7a22862a36eaafa851f46232985ed
KIMURA Tetsuro <haribote.nobody@gmail.com>
2020-05-21 23:13:43 +0900

    feat: <PictureItem />

--- src/components/PictureItem.tsx
+++ src/components/PictureItem.tsx
@@ -1,17 +1,35 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+import styled from 'styled-components'
 import { PromisifiedAxiosResponse } from '../@types/repositories'
+
+const ListItem = styled.li`
+  width: 128px;
+  height: 128px;
+`
+
+const Image = styled.img`
+  object-fit: contain;
+`
 
 export const PictureItem: React.FC<{
   fetchThumb: (key: string) => PromisifiedAxiosResponse<ArrayBuffer>
   pictureKey: string
 }> = ({ fetchThumb, pictureKey }) => {
+  const [src, setSrc] = useState<string>(
+    'data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=='
+  )
   useEffect(() => {
     const _fetchThumb = async (): Promise<void> => {
       const res = await fetchThumb(pictureKey)
-      console.log('fetchThumb()', pictureKey, res.data)
+      const blob = new Blob([res.data], { type: 'application/octet-binary' })
+      setSrc(URL.createObjectURL(blob))
     }
     _fetchThumb()
-  }, [fetchThumb, pictureKey])
+  }, [fetchThumb, pictureKey, src])
 
-  return <li>{pictureKey}</li>
+  return (
+    <ListItem>
+      <Image src={src} alt="" />
+    </ListItem>
+  )
 }
--- src/components/PictureList.tsx
+++ src/components/PictureList.tsx
@@ -1,17 +1,25 @@
 import React from 'react'
+import styled from 'styled-components'
 import { PromisifiedAxiosResponse } from '../@types/repositories'
 import { FlexWrapper } from './index'
 import { PictureItem } from './PictureItem'
+
+const List = styled.ul`
+  list-style: none;
+  display: flex;
+  margin: 0;
+  padding: 0;
+`
 
 export const PictureList: React.FC<{
   fetchThumb: (key: string) => PromisifiedAxiosResponse<ArrayBuffer>
   items: SourceRepository.ListItem[]
 }> = ({ fetchThumb, items }) => (
   <FlexWrapper>
-    <ul>
+    <List>
       {items.map(({ Key }) => (
         <PictureItem fetchThumb={fetchThumb} key={Key} pictureKey={Key} />
       ))}
-    </ul>
+    </List>
   </FlexWrapper>
 )
