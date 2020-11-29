060bf22709d06f8f84b58cc30a7205c6e7dc3363
KIMURA Tetsuro <haribote.nobody@gmail.com>
2020-11-29 13:40:50 +0900

    feat: `onClickDialogOpener()`

--- src/views/Field.tsx
+++ src/views/Field.tsx
@@ -1,8 +1,9 @@
 import { Button, Icon } from '@contentful/forma-36-react-components'
 import { FieldExtensionSDK } from 'contentful-ui-extensions-sdk'
-import React from 'react'
+import React, { useCallback } from 'react'
 import styled from 'styled-components'
 import { useAutoResize } from '../hooks/useAutoResize'
+import { useFieldValue } from '../hooks/useFieldValue'
 
 const ButtonLabel = styled.div`
   display: inline-flex;
@@ -11,20 +12,31 @@ const ButtonLabel = styled.div`
 `
 
 export const Field: React.FC<{ sdk: FieldExtensionSDK }> = ({ sdk }) => {
-  const onClickDialogOpener = (): Promise<void> =>
-    sdk.dialogs
-      .openCurrentApp({
-        title: 'Picture Selector',
-        width: 'medium',
-      })
-      .then(({ objectKey }: { objectKey: string }) => console.log(objectKey))
+  const [value, pushValue] = useFieldValue(sdk)
+  const onClickDialogOpener = useCallback(
+    () =>
+      sdk.dialogs
+        .openCurrentApp({
+          title: 'Picture Selector',
+          width: 'medium',
+        })
+        .then(({ objectKey }: { objectKey: string }) => {
+          pushValue({ key: objectKey })
+        }),
+    [sdk, pushValue]
+  )
 
   useAutoResize(sdk)
 
-  console.log('current value', sdk.field.getValue())
+  console.log('value', value)
 
   return (
     <React.Fragment>
+      <ul>
+        {value.map(({ key }) => (
+          <li key={key}>{key}</li>
+        ))}
+      </ul>
       <Button buttonType="muted" size="small" onClick={onClickDialogOpener}>
         <ButtonLabel>
           <Icon color="muted" icon="Asset" />
