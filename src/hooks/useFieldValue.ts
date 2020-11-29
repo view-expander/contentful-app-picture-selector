24c3743247460b69c3a6e35bac7495c29e35d561
KIMURA Tetsuro <haribote.nobody@gmail.com>
2020-11-29 15:08:53 +0900

    feat: `useFieldValue()`, `removeItem()`

--- src/hooks/useFieldValue.ts
+++ src/hooks/useFieldValue.ts
@@ -1,17 +1,33 @@
 import { FieldExtensionSDK } from 'contentful-ui-extensions-sdk'
-import { useEffect, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 
 export const useFieldValue = (
   sdk: FieldExtensionSDK
-): [ItemList, (key: string) => Promise<ItemList>] => {
-  const getValidValue = (value: ItemList | undefined) => Array.isArray(value) ? value : []
+): [
+  ItemList,
+  (key: string) => Promise<ItemList>,
+  (key: string) => Promise<ItemList>
+] => {
+  const getValidValue = (value: ItemList | undefined) =>
+    Array.isArray(value) ? value : []
 
   const currentValue = sdk.field.getValue() as ItemList | undefined
   const [value, setValue] = useState<ItemList>(getValidValue(currentValue))
 
+  const pushItem = useCallback(
+    (key: string) => sdk.field.setValue([...value, key]),
+    [sdk, value]
+  )
+  const removeItem = useCallback(
+    (key: string) => sdk.field.setValue(value.filter((k) => k !== key)),
+    [sdk, value]
+  )
+
   useEffect(() => {
-    sdk.field.onValueChanged((value: ItemList) => setValue(getValidValue(value)))
+    sdk.field.onValueChanged((value: ItemList) =>
+      setValue(getValidValue(value))
+    )
   }, [sdk])
 
-  return [value, (key: string) => sdk.field.setValue([...value, key])]
+  return [value, pushItem, removeItem]
 }
